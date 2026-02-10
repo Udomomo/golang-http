a7bc6f3fd3819e49c19b005ac68812f4eacd5792
Udomomo <udomomo@gmail.com>
2026-02-10 13:14:06 +0900

    add /time

--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/time", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/plain")
+
+		timezone, err := time.LoadLocation("Asia/Tokyo")
+    if err != nil {
+        panic(err)
+    }
+		fmt.Fprint(w, time.Now().In(timezone).Format("2006/01/02 15:04:05"))
+	})
+
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Add("Content-Type", "text/plain")
 		fmt.Fprintf(w, "Server is running!")
