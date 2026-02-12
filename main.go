63fe7291912de6b7135799b35945b19b38f4bb80
Udomomo <udomomo@gmail.com>
2026-02-12 23:10:58 +0900

    remove sample endpoint

--- main.go
+++ main.go
@@ -66,15 +66,6 @@ func main() {
 		fmt.Fprint(w, time.Now().In(timezone).Format("2006/01/02 15:04:05"))
 	}))
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("Content-Type", "text/plain")
-		fmt.Fprintf(w, "Server is running!")
-	})
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("Content-Type", "text/plain")
-		fmt.Fprintf(w, "Top page")
-	})
-
 	var h http.Handler = mux
 	if err := http.ListenAndServe("localhost:3000", h); err != nil {
 		fmt.Println("ListenAndServe error: ", err)
